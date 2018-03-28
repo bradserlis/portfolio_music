4d61738734d90ebead192ee5b88f7fb2ab008fec
bradserlis <bradserlis@yahoo.com>
2018-03-28 14:44:20 -0700

    commit1 - added band projects, consolidate css, created directories

---
+++ MyUnsaidEverything/mue.js
@@ -0,0 +1,27 @@
+var songTitle = document.querySelector("#songTitle")
+var jumbotron = document.querySelector(".jumbotron")
+
+
+//make "Bodies in the Dining Room" (.songTitle) toggle visibility when jumbotron is hovered over
+
+
+
+
+
+
+
+
+
+
+
+
+// jumbotron.addEventListener("mouseover", function(){
+//     width: 570px;
+//     height: 500px;
+//     padding: 100px;
+//     border-radius: 50px;
+
+
+// })
+
+//
